3f494acebe5db6589e6d262d7fa1fb0953773342
Tiago Danin <TiagoDanin@outlook.com>
2025-02-01 17:37:41 -0300

    chore: Configure Next.js static export and base path for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
   reactStrictMode: true,
 };
 
